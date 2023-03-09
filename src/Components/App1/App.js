7734570b03cb44418fc7bde457e7fd836c326b21
ETurner-Bisset <119737150+ETurner-Bisset@users.noreply.github.com>
2023-03-09 15:15:46 +0000

    Step 40

--- src/Components/App1/App.js
+++ src/Components/App1/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults'
 import Playlist from './Playlist/Playlist'
+// import TrackList from '../TrackList/TrackList';
 
 class App extends React.Component {
 
@@ -12,9 +13,24 @@ class App extends React.Component {
     this.state = {
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playlistName: 'My Playlist',
+      playlistTracks :[{name: 'playlistName4', artist: 'playlistArtist4', album: 'playlistAlbum4', id: 4},
+      {name: 'playlistName5', artist: 'playlistArtist5', album: 'playlistAlbum5', id: 5},
+      {name: 'playlistName6', artist: 'playlistArtist6', album: 'playlistAlbum6', id: 6}]
+
     }
+    // this.addTrack = this.addTrack.bind(this);
   }
+
+  // addTrack(track) {
+    // let tracks = this.state.playlistTracks;
+    // if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      // return;
+    // }
+    // tracks.push(track);
+    // this.setState({playlistTracks: tracks});
+  // }
 
   render() {
     return (
@@ -23,8 +39,9 @@ class App extends React.Component {
       <div className="App">
       <SearchBar />
     <div className="App-playlist">
-      <SearchResults searchResults={this.state.searchResults}/> 
-       <Playlist /> 
+      <SearchResults searchResults={this.state.searchResults}
+      onAdd={this.addTrack}/> 
+       <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlisrTracks}/> 
     </div>
   </div>
 </div>
