86d23d227e058d7738eff96be1d2f2e5c8b70e4b
ETurner-Bisset <119737150+ETurner-Bisset@users.noreply.github.com>
2023-03-14 14:37:38 +0000

    Jammming step 73

--- src/Components/App1/App.js
+++ src/Components/App1/App.js
@@ -22,6 +22,9 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -30,26 +33,43 @@ class App extends React.Component {
       return;
     }
     tracks.push(track);
-    // this.setState({playlistTracks: tracks});
+    this.setState({playlistTracks: tracks});
   }
 
-  // removeTrack(track) {
-  //   let tracks = this.state.playlistTracks;
-  //   tracks = tracks.filter(currentTrack.id => currentTrack.id !== track.id);
-  //   this.setState({playlistTracks : tracks});
-  // }
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({playlistTracks : tracks});
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+  savePlaylist() {
+    // alert("This method is linked to the button correctly")
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(term) {
+    console.log(term);
+  }
 
   render() {
     return (
       <div>
        <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-      <SearchBar />
+      <SearchBar onSearch={this.search}/>
     <div className="App-playlist">
       <SearchResults searchResults={this.state.searchResults}
        onAdd={this.addTrack}  
        />   
-       <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} /> 
+       <Playlist playlistName={this.state.playlistName} 
+       playlistTracks={this.state.playlistTracks} 
+       onRemove={this.removeTrack}
+       onNameChange={this.updatePlaylistName}
+       onSave={this.savePlaylist}/> 
     </div>
   </div>
 </div>
